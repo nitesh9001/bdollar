6770c591b4ddfaad9291e0ca9c1fe90e6d031da5
inter <intern@cybersrcc.com>
2020-07-07 12:13:30 +0530

    Updates

---
+++ precache-manifest.202c494eb853e2bd403f3cd13c2027ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8844b9b5e13bbeafc9febf38b4ebd2b0",
+    "url": "/bdollar/index.html"
+  },
+  {
+    "revision": "ce746d05a0238ed2b77b",
+    "url": "/bdollar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34d3bc7926263f4329d6",
+    "url": "/bdollar/static/js/2.8df3040a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bdollar/static/js/2.8df3040a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce746d05a0238ed2b77b",
+    "url": "/bdollar/static/js/main.1673af4b.chunk.js"
+  },
+  {
+    "revision": "8819665c61243493a1de",
+    "url": "/bdollar/static/js/runtime-main.4f54c3d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bdollar/static/media/logo.5d5d9eef.svg"
+  }
+]);
